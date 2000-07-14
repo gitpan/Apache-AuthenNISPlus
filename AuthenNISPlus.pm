f2ba504d844ae751f99ab1312313d77f61174177
Valerie Delane <valerie@cpan.org>
2000-07-14 21:41:19 +0000

    Import of VALERIE/Apache-AuthenNISPlus-0.06 from CPAN.
    gitpan-cpan-distribution: Apache-AuthenNISPlus
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         VALERIE/Apache-AuthenNISPlus-0.06.tar.gz
    gitpan-cpan-author:       VALERIE
    gitpan-cpan-maturity:     released

--- AuthenNISPlus.pm
+++ AuthenNISPlus.pm
@@ -5,126 +5,124 @@ use Apache::Constants qw/:common/;
 use vars qw/%ENV/;
 use Net::NISPlus::Table;
 
-$Apache::AuthenNISPlus::VERSION = '0.04';
+$Apache::AuthenNISPlus::VERSION = '0.06';
 my $self="Apache::AuthenNISPlus";
 
 sub handler {
 
-  # get request object
-  my $r = shift;
+   # get request object
+   my $r = shift;
 
-  # service only the first internal request
-  return OK unless $r->is_initial_req;
+   # service only the first internal request
+   return OK unless $r->is_initial_req;
 
-  # get password user entered in browser
-  my($res, $sent_pwd) = $r->get_basic_auth_pw;
+   # get password user entered in browser
+   my ($res, $sent_pwd) = $r->get_basic_auth_pw;
 
-  # decline if not basic
-  return $res if $res;
+   # decline if not basic
+   return $res if $res;
 
-  # get user name
-  my $name = $r->connection->user;
+   # get user name
+   my $name = $r->connection->user;
 
-  # be sure username is reasonable
-  $name=~m/(\w+)/; $name=$1;
+   # blank user name would cause problems
+   unless($name){
+      $r->note_basic_auth_failure;
+      $r->log_reason($self . ': no username supplied', $r->uri);
+      return AUTH_REQUIRED;
+   }
 
-  # blank user name would cause problems
-  unless($name){
-    $r->note_basic_auth_failure;
-    $r->log_reason("$self: no username supplied", $r->uri);
-    return AUTH_REQUIRED;
-  }
+   # load apache config vars
+   my $dir_config = $r->dir_config;   
 
-  # load apache config vars
-  my $dir_config = $r->dir_config;   
+   # get passwd table name
+   my $passwd_table = $dir_config->get('NISPlus_Passwd_Table');
 
-  # get passwd table name
-  my $passwd_table = $dir_config->get("NISPlus_Passwd_Table");
+   # get user password entry
+   my $pwd_table = Net::NISPlus::Table->new($passwd_table);
+   unless ($pwd_table){
+      $r->note_basic_auth_failure;
+      $r->log_reason($self . ': cannot get nis+ passwd table', $r->uri);
+      return AUTH_REQUIRED;
+   }
 
-  # get passwd entry
-
-  # get user password entry
-  my $pwd_table = Net::NISPlus::Table->new($passwd_table);
-  unless ($pwd_table){
-    $r->note_basic_auth_failure;
-    $r->log_reason("$self: cannot get nis+ passwd table", $r->uri);
-    return AUTH_REQUIRED;
-  }
-  my $pwd = "";
-  my $group = "";
-  foreach ($pwd_table->list()){
-    if(@{$_}[0] eq $name){
-      $pwd = @{$_}[1];
-      $group = @{$_}[3];
+   my $pwd = '';
+   my $group = '';
+   # look for name match
+   foreach ($pwd_table->lookup('[name=' . $name . ']')){
+      $pwd = $_->{'passwd'};
+      $group = $_->{'gid'};
       last;
-    }
-  }
+   }
 
-  unless($pwd){
-    $r->note_basic_auth_failure;
-    $r->log_reason(
-      "$self: user $name is not in $passwd_table", $r->uri
-    );
-    return AUTH_REQUIRED;
-  }
+   unless($pwd){
+      $r->note_basic_auth_failure;
+      $r->log_reason(
+         $self . ': user ' . $name . ' is not in ' . $passwd_table, $r->uri
+      );
+      return AUTH_REQUIRED;
+   }
 
-  #stash group id lookup for authorization check 
-  $r->notes($name."Group", $group);
-  unless(crypt($sent_pwd, $pwd) eq $pwd) {
-    $r->note_basic_auth_failure;
-    $r->log_reason(
-      "$self: user $name password does not match $passwd_table", $r->uri
-    );
-    return AUTH_REQUIRED;
-  }
-  $r->push_handlers(PerlAuthzHandler => \&authz);
-  return OK;
+   # stash group id lookup for authorization check 
+   $r->notes($name . 'Group', $group);
+
+   unless(crypt($sent_pwd, $pwd) eq $pwd) {
+      $r->note_basic_auth_failure;
+      $r->log_reason(
+         $self . ': user ' . $name . ' password does not match ' . 
+         $passwd_table, $r->uri
+      );
+      return AUTH_REQUIRED;
+   }
+   $r->push_handlers(PerlAuthzHandler => \&authz);
+   return OK;
 }
 
 sub authz {
  
- # get request object
-  my $r = shift;
-  my $requires = $r->requires;
-  return OK unless $requires;
+   # get request object
+   my $r = shift;
+   my $requires = $r->requires;
+   return OK unless $requires;
 
-  # get user name
-  my $name = $r->connection->user;
+   # get user name
+   my $name = $r->connection->user;
 
-  # get group table name
-  my $dir_config = $r->dir_config;   
-  my $group_table=$dir_config->get("NISPlus_Group_Table");
+   # get group table name
+   my $dir_config = $r->dir_config;   
+   my $group_table = $dir_config->get('NISPlus_Group_Table');
 
-  for my $req (@$requires) {
-    my($require, @rest) = split /\s+/, $req->{requirement};
+   for my $req (@$requires) {
+      my ($require, @rest) = split /\s+/, $req->{requirement};
 
-    #ok if user is simply authenticated
-    if($require eq "valid-user"){return OK}
+      # ok if user is simply authenticated
+      if ($require eq 'valid-user'){return OK}
 
-    # ok if user is one of these users
-    elsif($require eq "user") {return OK if grep $name eq $_, @rest}
+      # ok if user is one of these users
+      elsif ($require eq 'user') {return OK if grep $name eq $_, @rest}
 
-    # ok if user is member of a required group.
-    elsif($require eq "group") {
-      my $group_table = Net::NISPlus::Table->new($group_table);
-      unless ($group_table){
-        $r->note_basic_auth_failure;
-        $r->log_reason("$self: cannot get nis+ group table", $r->uri);
-        return AUTH_REQUIRED;
+      # ok if user is member of a required group.
+      elsif ($require eq 'group') {
+         my $group_table = Net::NISPlus::Table->new($group_table);
+         unless ($group_table){
+            $r->note_basic_auth_failure;
+            $r->log_reason($self . ': cannot get nis+ group table', $r->uri);
+            return AUTH_REQUIRED;
+         }
+         my %groups_to_gids;
+         foreach ($group_table->list()){$groups_to_gids{@{$_}[0]} = @{$_}[2]}
+         for my $group (@rest) {
+            next unless exists $groups_to_gids{$group};
+            return OK if $r->notes($name . 'Group') == $groups_to_gids{$group};
+         }
       }
-      my %groups_to_gids;
-      foreach ($group_table->list()){$groups_to_gids{@{$_}[0]} = @{$_}[2]}
-      for my $group (@rest) {
-        next unless exists $groups_to_gids{$group};
-        return OK if $r->notes($name."Group") == $groups_to_gids{$group};
-      }
-    }
-  }
-  $r->note_basic_auth_failure;
-  $r->log_reason(
-    "$self: user $name not member of required group in $group_table", $r->uri
-  );
-  return AUTH_REQUIRED;
+   }
+   $r->note_basic_auth_failure;
+   $r->log_reason(
+      $self . ': user ' . $name . ' not member of required group in ' . 
+      $group_table, $r->uri
+   );
+   return AUTH_REQUIRED;
 }
 
 1;
@@ -169,6 +167,6 @@ it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-mod_perl(3), Apache(3), nismatch(1)
+mod_perl(3), Apache(3).
 
 =cut
